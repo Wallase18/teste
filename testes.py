573921edda49c037c0566ab9b093fe4abd302ced
Wallase Alan Costa De Morais <wallacealanmorais@gmail.com>
2022-05-02 19:23:32 -0300

    Acesso a diretorio raiz do wordpress e lista de arquivos de uma pasta

---
+++ testes.py
@@ -0,0 +1,8 @@
+import os
+import subprocess
+import json
+
+# pasta = r'C:\Users\BrasoDev\Downloads\Hemobras\06.06.2017PassinhoCertoterceiraedicao'
+# RaizWord = r'C:\xampp\htdocs\testes'
+# jsont = r'C:/Users/BrasoDev/Downloads/teste/b.json'
+# os.chdir(RaizWord)
